2f3f0e3030799f5f4caaf4370b57978055a5f40b
sabrimansor <sabrimansor@users.noreply.github.com>
2024-04-23 18:07:49 +0000

    deployed via Gatsby Publish Action 🎩 for e1286566b51e06fb68cac16152bb0f32aa84b9d1

---
+++ 756-496650bd43a96e8760a4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksabrimansor_website=self.webpackChunksabrimansor_website||[]).push([[756],{8756:function(e,s,c){s.default=c.p+"static/Feather-f780c210ecac8211626013242c3ecdfc.svg"}}]);
+//# sourceMappingURL=756-496650bd43a96e8760a4.js.map
